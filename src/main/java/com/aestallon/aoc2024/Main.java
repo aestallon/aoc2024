c5ceca42713a15784dc3687b1a192bbf719a0c50
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2024-12-01 21:56:56 +0100

    initial commit

---
+++ src/main/java/com/aestallon/aoc2024/Main.java
@@ -0,0 +1,12 @@
+package com.aestallon.aoc2024;
+
+public final class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+  
+  public static int two() {
+    return 2;
+  }
+}
---
+++ src/test/java/com/aestallon/aoc2024/MainTest.java
@@ -0,0 +1,14 @@
+package com.aestallon.aoc2024;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+  
+  @Test
+  void dummyMethodWorks() {
+    assertThat(Main.two()).isEqualTo(2);
+  }
+
+}
